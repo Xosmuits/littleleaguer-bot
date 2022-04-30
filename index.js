df83a7260a39c1742ea941207b22871884ea0c0b
Xosmuits <loveofbiersack@hotmail.com>
2022-04-30 23:46:48 +0300

    js

--- index.js
+++ index.js
@@ -1,31 +1,69 @@
 const Discord = require("discord.js")
-
 require("dotenv").config()
+
+const generateImage = require("./generateImage")
 
 const client = new Discord.Client({
     intents: [
          "GUILDS",
-         "GUILD_MESSAGES"
+         "GUILD_MESSAGES",
+         "GUILD_MEMBERS"
     ]
 })
 
 let bot = {
-    client
+    client,
+    prefix: "l.",
+    owners: ["226755446537584650"]
 }
 
-client.on("ready", () => {
-    console.log(`${client.user.tag} is now online`)
+client.commands = new Discord.Collection()
+client.events = new Discord.Collection()
+
+client.loadEvents = (bot, reload) => require("./handlers/events")(bot, reload)
+client.loadCommands = (bot, reload) => require("./handlers/commands")(bot, reload)
+
+
+client.loadEvents(bot, false)
+client.loadCommands(bot, false)
+
+module.exports = bot
+
+
+client.slashcommands = new Discord.Collection()
+ 
+client.loadSlashCommands = (bot, reload) => require("./handlers/slashcommands")(bot, reload)
+client.loadSlashCommands(bot, false)
+
+client.on("interactionCreate", (interaction) => {
+    if (!interaction.isCommand()) return
+    if (!interaction.inGuild()) return interaction.reply("This command can only be used in a server")
+
+    const slashcmd = client.slashcommands.get(interaction.commandName)
+
+    if (!slashcmd) return interaction.reply("Invalid slash command")
+
+    if (slashcmd.perms && !interaction.member.permissions.has(slashcmd.perms))
+        return interaction.reply("You do not have permission for this command")
+
+    slashcmd.run(client, interaction)
 })
 
-//
-//client.on("messageCreate", (message) => {
-    //if (message.content == "hi"){
-    //    message.reply("as")
-  //  }
 
-//})
+const welcomeChannelId = "585550060444450836"
+
+// IMAGE WELCOME
+client.on("guildMemberAdd", async (member) => {
+    const img = await generateImage(member)
+    member.guild.channels.cache.get(welcomeChannelId).send({
+        content: `<@${member.id}> Welcome to the server!`,
+        files: [img]
+    })
+})
+ 
+
+
+
 
 client.login(process.env.TOKEN)
-
-const TOKEN ="OTY5NTc2MzAxMzUyNjExODgw.YmvaTA.G0eQm35wMTi8mJFnnetZStzITrs"
 
